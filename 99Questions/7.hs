488d4512a74194d4a5424987899e558d4cd25202
Andhieka Putra <putra@garena.com>
2015-08-04 19:07:59 +0800

    Solve until question 15

--- 99Questions/7.hs
+++ 99Questions/7.hs
@@ -35,3 +35,57 @@ pack' (x:xs) = if x `elem` (head (pack' xs))
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode xs = map (\ g -> ((length g), (head g))) ys
     where ys = pack xs
+
+-- Question 11
+
+data CodeItem a = Single a | Multiple Int a deriving (Show)
+codeItem :: (Eq a) => Int -> a -> CodeItem a
+codeItem n x
+    | n <= 0    = error "CodeItem must have positive number!"
+    | n == 1    = Single x
+    | otherwise = Multiple n x
+
+encodeModified :: (Eq a) => [a] -> [CodeItem a]
+encodeModified xs =
+    let ys = encode xs
+    in map (\(n, x) -> if n == 1 then Single x else Multiple n x) ys
+
+-- Question 12
+
+decodeModified :: (Eq a) => [CodeItem a] -> [a]
+decodeModified ((Single x):xs) = x:(decodeModified xs)
+decodeModified ((Multiple n x):xs) = (replicate n x) ++ (decodeModified xs)
+decodeModified [] = []
+
+-- Question 13
+
+encodeDirectHelper :: (Eq a) => Int -> [a] -> [CodeItem a]
+encodeDirectHelper n [x] = [codeItem (n+1) x]
+encodeDirectHelper n (x:xs@(y:_))
+    | x == y    = encodeDirectHelper (n+1) xs
+    | otherwise = [ci] ++ encodeDirectHelper 0 xs
+        where ci = codeItem (n+1) x
+
+encodeDirect :: (Eq a) => [a] -> [CodeItem a]
+encodeDirect xs = encodeDirectHelper 0 xs
+
+
+-- Question 14
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = (replicate 2 x) ++ (dupli xs)
+
+-- Question 15
+
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli _ 0 = []
+repli (x:xs) n = (replicate n x) ++ (repli xs n)
+
+-- Question 16
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery _ 0 = []
+dropEvery 
